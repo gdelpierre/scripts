e3c679621e62770119712b520a5e8d9d409174b6
Guillaume Delpierre <gde@llew.me>
2014-07-25 15:47:38 +0200

    Update bl_wp_ip.sh

--- bl_wp_ip.sh
+++ bl_wp_ip.sh
@@ -14,16 +14,17 @@ readonly SORT=$(type -P sort)
 readonly GREP=$(type -P grep)
 readonly CURL=$(type -P curl)
 readonly GIT=$(type -P git)
+readonly RM=$(type -P rm)
 
 usage()
 {
-        echo "$0: days occurence"
+	echo "$0: days occurence"
 }
 
 # Check if number of arguments is correct.
 if [ "$#" -ne "2" ]; then
-        usage
-        exit 1
+	usage
+	exit 1
 fi
 
 # Define global used variables.
@@ -31,13 +32,17 @@ SEARCH_DAY=${1}
 RECCURENCE_IP=${2}
 
 # Temporary files.
-readonly TMP_FILE="/tmp/parse.txt"
-readonly TMP_BF_LIST="/tmp/bruteforce.txt"
+readonly TMP_FILE="/tmp/tmp_bl_parse.txt"
+readonly TMP_SORT_FILE="/tmp/tmp_bl_sort.txt"
+readonly TMP_BF_LIST="/tmp/tmp_bl_bruteforce.txt"
+
+# Git Blacklist file
+NEW_BL_FILE="/usr/local/bin/wp_ip_blacklist/wordpress_blacklist_ip.txt"
 
 # find IP trying to POST on wp-admin
 post_wp()
 {
-        local find_location="*/*/apache2/ */*/nginx/ */*/varnish/"
+        local find_location="/var/backups/*/*/{apache2,nginx,varnish}/"
         local find_opts="-mtime ${SEARCH_DAY} -name '*access*' -type f -print0"
         local xargs_opts="-0"
         local zgrep_opts="-h 'POST.*wp-login\.php'"
@@ -54,7 +59,7 @@ sort_ip()
 	local uniq_opts="-c"
 	${SORT} ${sort_opts} < "${TMP_FILE}" |
 	${UNIQ} ${uniq_opts} |
-	${SORT} ${sort_opts}
+	${SORT} ${sort_opts} > "${TMP_SORT_FILE}"
 }
 
 # Only keep highest reccurence of IPs.
@@ -64,7 +69,7 @@ keep_highest_ip_rate()
 		if [[ "$count" -ge "$RECCURENCE_IP" ]] ; then
 			echo "${ip}"
 		fi
-	done < "${TMP_FILE}" >> "${TMP_BF_LIST}"
+	done < "${TMP_SORT_FILE}" >> "${TMP_BF_LIST}"
 }
 
 # diff current bl and new list, keep first IP occurence, sort them and built
@@ -72,13 +77,14 @@ keep_highest_ip_rate()
 make_me_a_ragout()
 {
 	local sort_opts=(-u -n)
-	local git_url=""
+	local grep_opts="-v '^$'"
+	local git_url="https://raw.githubusercontent.com/Nexylan/wp_ip_blacklist/master/wordpress_blacklist_ip.txt"
 	local curl_opts="-s"
-	local current_bl=/tmp/current_bl.txt
-	local new_bl=/tmp/wordpress_blacklist_ip.txt
+	local current_bl=/tmp/tmp_bl_current.txt
 	${CURL} ${curl_opts} ${git_url} > ${current_bl}
 	${SORT} ${sort_opts[0]} ${current_bl} ${TMP_BF_LIST} |
-	${SORT} ${sort_opts[1]} > ${new_bl}
+	${GREP} ${grep_opts} |
+	${SORT} ${sort_opts[1]} > ${NEW_BL_FILE}
 }
 
 # Send new list.
@@ -86,7 +92,7 @@ send_list()
 {
 	local git_opts=('add' 'commit -m' 'push')
 	local today=$(date +%Y%m%d)
-	local commit_msg="Update blacklist - $today"
+	local commit_msg="\"Update blacklist - $today\""
 
 	# Test if git is installed.
 	[[ "$GIT" ]] ||
@@ -96,9 +102,14 @@ send_list()
 	[[ -d .git ]] || echo -e "Not a git repo"
 
 	${GIT} ${git_opts[0]} ${NEW_BL_FILE} &&
-	${GIT} ${git_opts[1]} ${commit_msg} &&
+	eval ${GIT} ${git_opts[1]} ${commit_msg} &&
 	${GIT} ${git_opts[2]} || exit 1
+}
 
+clean_tmp_files()
+{
+	local tmp_file="/tmp/tmp_bl_*"
+	${RM} ${tmp_file}
 }
 
 post_wp
@@ -106,5 +117,6 @@ sort_ip
 keep_highest_ip_rate
 make_me_a_ragout
 send_list
+clean_tmp_files
 
 exit 0
