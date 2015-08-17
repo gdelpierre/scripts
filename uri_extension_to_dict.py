f2219b74c6dc969581fdd9d8743a1f2aad1621e8
Guillaume 'Llew' Delpierre <gde@llew.me>
2015-08-17 10:46:32 +0200

    parse log file and count extension to dict

---
+++ uri_extension_to_dict.py
@@ -0,0 +1,23 @@
+#! /usr/bin/env python2.7
+
+import re
+from collections import defaultdict
+from sys import argv
+
+def count_extension(filename):
+    with open(filename, 'r') as file:
+        ext_dict = defaultdict(int)
+        for line in file:
+            line = line.strip()
+            if line:
+                ext_line = line.split('|')[3]
+                if '.' in ext_line:
+                  clean_ext = re.search('[a-z]+', \
+                              ext_line.split('.')[-1])
+                  if clean_ext is not None:
+                    ext_dict[clean_ext.group(0)] += 1
+
+    print ext_dict
+
+if __name__ == '__main__':
+    count_extension(argv[1])
